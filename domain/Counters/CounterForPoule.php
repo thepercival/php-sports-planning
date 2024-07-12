003124ccb2eb36bc23f8025785f8044abfac94b4
thepercival <coendunnink@gmail.com>
2024-07-12 14:12:04 +0200

    fix(syntax): remove error

---
+++ domain/Counters/CounterForPoule.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SportsPlanning\Counters;
+
+use SportsHelpers\Counter;
+use SportsPlanning\Poule;
+
+/**
+ * @template-extends Counter<Poule>
+ */
+readonly class CounterForPoule extends Counter
+{
+    public function __construct(Poule $poule, int $count = 0)
+    {
+        parent::__construct($poule, $count);
+    }
+
+    public function getPoule(): Poule
+    {
+        return $this->countedObject;
+    }
+
+    public function increment(): CounterForPoule
+    {
+        return new CounterForPoule($this->countedObject, $this->count + 1 );
+    }
+
+    public function decrement(): CounterForPoule
+    {
+        return new CounterForPoule($this->countedObject, $this->count - 1 );
+    }
+
+    public function __toString(): string
+    {
+        return $this->getPoule()->getNumber() . ' ' . $this->count() . 'x';
+    }
+}
