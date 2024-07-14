aafff4f0b40b8df79c12094de19163a0139b380f
thepercival <coendunnink@gmail.com>
2024-07-14 07:40:53 +0200

    fix(schedule): restructure counters

--- domain/Counters/Maps/Schedule/RangedPlaceCounterMap.php
+++ domain/Counters/Maps/Schedule/RangedPlaceCounterMap.php
@@ -1,24 +1,19 @@
 <?php
 
-namespace SportsPlanning\Counters\Maps;
+namespace SportsPlanning\Counters\Maps\Schedule;
 
-use SportsPlanning\Combinations\Amount\Calculator as AmountCalculator;
 use SportsPlanning\Combinations\Amount\Range as AmountRange;
 use SportsPlanning\Counters\Maps\PlaceCounterMap as PlaceCounterMapBase;
 use SportsPlanning\Counters\Reports\RangedPlaceCountersReport;
 use SportsPlanning\Place;
 
- class RangedPlaceCounterMap
+class RangedPlaceCounterMap
 {
-    private readonly RangedPlaceCountersReport $report;
-    private PlaceCounterMapBase $map;
-
     private readonly AmountRange $allowedRange;
 
-    public function __construct(PlaceCounterMapBase $map, AmountRange $allowedRange) {
+    public function __construct(private AmountCounterMap|HomeCounterMap $map, AmountRange $allowedRange) {
         $this->map = $map;
         $this->allowedRange = $allowedRange;
-        $this->report = new RangedPlaceCountersReport($map, $allowedRange);
     }
 
     public function getAllowedRange(): AmountRange {
@@ -39,14 +34,9 @@ use SportsPlanning\Place;
         $this->map->removePlace($place);
     }
 
-    public function getNrOfPlacesBelowMinimum(): int
+    public function calculateReport(): RangedPlaceCountersReport
     {
-        return $this->report->getNrOfPlacesBelowMinimum();
-    }
-
-    public function getNrOfPlacesAboveMaximum(): int
-    {
-        return $this->report->getNrOfPlacesAboveMaximum();
+        return new RangedPlaceCountersReport($this->map, $this->allowedRange);
     }
 
 
@@ -61,37 +51,6 @@ use SportsPlanning\Place;
         return array_key_exists($amount, $amountMap) ? $amountMap[$amount]->count : 0;
     }
 
-    public function getAmountDifference(): int
-    {
-        return $this->report->getAmountDifference();
-    }
-
-    public function getRange(): AmountRange|null
-    {
-        return $this->report->getRange();
-    }
-
-    public function getMinAmount(): int
-    {
-        return $this->report->getMinAmount();
-    }
-
-
-    public function getCountOfMinAmount(): int
-    {
-        return $this->report->getCountOfMinAmount();
-    }
-
-    public function getMaxAmount(): int
-    {
-        return $this->report->getMaxAmount();
-    }
-
-    public function getCountOfMaxAmount(): int
-    {
-        return $this->report->getCountOfMaxAmount();
-    }
-
     public function withinRange(int $nrOfCombinationsToGo): bool
     {
         return $this->minimumCanBeReached($nrOfCombinationsToGo) && !$this->aboveMaximum($nrOfCombinationsToGo);
@@ -99,15 +58,16 @@ use SportsPlanning\Place;
 
     public function minimumCanBeReached(int $nrOfCombinationsToGo): bool
     {
-        if( $this->getNrOfPlacesBelowMinimum() <= $nrOfCombinationsToGo ) {
+        $report = $this->calculateReport();
+        if( $report->getNrOfPlacesBelowMinimum() <= $nrOfCombinationsToGo ) {
             return true;
         };
 
         $allowedMin = $this->allowedRange->getMin();
         $nrOfPossibleCombinations = $this->getMap()->count();
 
-        if ( $this->getMinAmount() === $allowedMin->amount
-            && $this->getCountOfMinAmount() + $nrOfCombinationsToGo <= $nrOfPossibleCombinations
+        if ( $report->getMinAmount() === $allowedMin->amount
+            && $report->getCountOfMinAmount() + $nrOfCombinationsToGo <= $nrOfPossibleCombinations
         ) {
             return true;
         }
@@ -116,17 +76,18 @@ use SportsPlanning\Place;
 
     public function aboveMaximum(int $nrOfCombinationsToGo): bool
     {
-        if( $this->getNrOfPlacesAboveMaximum() === 0 ) {
+        $report = $this->calculateReport();
+        if( $report->getNrOfPlacesAboveMaximum() === 0 ) {
             return false;
         }
 
         $allowedMax = $this->allowedRange->getMax();
         $nrOfPossibleCombinations = $this->getMap()->count();
 
-        if ( $this->getMaxAmount() === $allowedMax->amount
+        if ( $report->getMaxAmount() === $allowedMax->amount
             &&
             (
-            $this->getCountOfMaxAmount() + $nrOfCombinationsToGo <= $nrOfPossibleCombinations
+                $report->getCountOfMaxAmount() + $nrOfCombinationsToGo <= $nrOfPossibleCombinations
             )
         ) {
             return false;
